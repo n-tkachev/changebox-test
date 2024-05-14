890ebb7393cf771e192ca7f4069897ab7ef7e7e8
Nick Tkachev <125368ap@gmail.com>
2024-05-14 18:25:37 +0200

    adding dist for gh-pages

--- src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export function fileToBase64(file: File, callback: (base64String: string) => void): void {
-    const reader = new FileReader();
-    reader.onload = function(event) {
-        if (typeof event.target?.result === 'string') {
-            callback(event.target.result);
-        }
-    };
-    reader.readAsDataURL(file);
-}
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <div>
-    <h2>{{ $t("message.home.header") }}</h2>
-  </div>
-</template>
